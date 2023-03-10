7c1a5af6ad1131e0f51d12eefcf683663ffd9faf
DrDevoo <krichard001@icloud.com>
2023-03-10 20:08:52 +0100

    f

--- routes/orders.js
+++ routes/orders.js
@@ -116,11 +116,6 @@ router.post("/finish/:id", async (req,res) =>{
      }
 })
 
-     const storeItems = new Map([
-          [1, { priceInCents: 10000, name: "Learn React Today" }],
-          [2, { priceInCents: 20000, name: "Learn CSS Today" }],
-        ])
-
 router.post("/pay", async (req, res) => {
   try {
      console.log("Fiezetés megkezdve!")
@@ -129,7 +124,6 @@ router.post("/pay", async (req, res) => {
       payment_method_types: ["card"],
       mode: "payment",
       line_items: req.body.items.map(item => {
-          const storeItem = storeItems.get(item.id)
           return {
             price_data: {
               currency: "huf",
