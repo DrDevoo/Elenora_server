16b9c3340f99dedb9696c58100747860725f77cb
K_Richard <119009903+DrDevoo@users.noreply.github.com>
2023-04-14 22:05:33 +0200

    u

--- routes/collections.js
+++ routes/collections.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const ProdCollection = require('../models/collections-model');
-const uploadController = require("./imgcontroller");
+
 
 //Autentikalt index oldal
 router.get("/", async (req, res) => {
