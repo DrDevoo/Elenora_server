af91a8070540423f10da76e4b3daf60b089d93fa
K_Richard <119009903+DrDevoo@users.noreply.github.com>
2023-07-11 15:58:44 +0200

    Update logs.js

--- routes/logs.js
+++ routes/logs.js
@@ -24,7 +24,7 @@ async function get(action){
      try{
           var query = Logs.find({action:action})
           console.log(query)
-          console.log(query.count())
+
           return query.count()
    }catch(err){
         console.log('Log sikertelen lekérése!!!')
